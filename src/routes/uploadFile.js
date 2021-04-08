e335fffec3095bada18d7fcdf00ed2cdfc4a7c3b
Leonel Rodriguez <leonellrodriguez@gmail.com>
2021-04-08 13:23:33 -0500

    Campos para HTTPS

--- src/routes/uploadFile.js
+++ src/routes/uploadFile.js
@@ -10,31 +10,35 @@ const s3 = new AWS.S3({
   secretAccessKey: config.AWS_Secret_access_key
 })
 
-let storage = multer.diskStorage({
-  destination:(req, file, cb) => {
-    cb(null, './subidas')
-  },
-  filename:(req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-  }
-})
-
-const upload = multer({ storage })
-
 
 fileRoutes.get('/file', (request, response) => {
   response.sendFile(__dirname + "/upload.html")
 })
 
 
-fileRoutes.post('/file', upload.single('idUrl') ,(request, response) => {
+// destination:(req, file, cb) => {
+//   cb(null, './subidas')
+// },
+let storage = multer.diskStorage({
+  destination: path.join(__dirname, '../../subidas'),
+  filename:(req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+
+const upload = multer({ 
+  storage 
+}).single('idUrl')
+
+fileRoutes.post('/file', upload ,(request, response) => {
   console.log(`Store location is ${request.hostname}/${request.file.path}`)
   uploadS3(request.file, request.file.filename, request.file.path)
   return response.send(request.file)
 })
 
 const uploadS3 = (file, fileName, filePath) => {
-  fs.readFile('subidas\\' + fileName, (err, data) => {
+  console.log(__dirname + '../../subidas/' + fileName);
+  fs.readFile(__dirname + '../../subidas/' + fileName, (err, data) => {
     if(err) throw err
     const paramsPut = {
       Bucket: 'finanservs',
