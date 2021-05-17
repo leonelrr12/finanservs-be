14acbfb919bf30646d7359b7a6f1796de6e4648d
Leonel Rodriguez <leonellrodriguez@gmail.com>
2021-05-17 14:16:11 -0500

    Pruebas

--- src/data/Maribet.js
+++ src/data/Maribet.js
@@ -9,9 +9,9 @@ const Datos = () => {
         formaPago:'PAGOS VOLUNTARIOS',
         contometro:'2121212211',
         referencia:'2018179867',
-        score:'0'},
-       
-       {institucion:'BANCO GENERAL',
+        score: '0'},
+
+        {institucion:'BANCO GENERAL',
         tipoObligacion:'PREST. PERSONAL',
         saldoActual:'3686.16',
         letra:'101.98',
@@ -20,18 +20,18 @@ const Datos = () => {
         contometro:'111111111111111',
         referencia:'2018734205',
         score:'0'},
-       
+    
         {institucion:'BANISTMO, S.A.',
         tipoObligacion:'HIPOTECA',
         saldoActual:'10495.79',
         letra:'121.35',
         diasAtraso:'0',
         formaPago:'DESCUENTO DIRECTO',
-        contometro:'111111111777111111111111',
+        contometro:'111111111111111111111',
         referencia:'2004626981',
         score:'0'},
-       
-       {institucion:'SERVICIOS FINANCIEROS INMEDIATOS, S.A.',
+    
+        {institucion:'SERVICIOS FINANCIEROS INMEDIATOS, S.A.',
         tipoObligacion:'PREST. PERSONAL',
         saldoActual:'16112.08',
         letra:'147.94',
@@ -40,8 +40,8 @@ const Datos = () => {
         contometro:'1111111',
         referencia:'2019232803',
         score:'0'},
-       
-       {institucion:'ADELANTOS CAPITAL CORP',
+    
+        {institucion:'ADELANTOS CAPITAL CORP',
         tipoObligacion:'PREST. PERSONAL',
         saldoActual:'451.49',
         letra:'100.69',
@@ -50,8 +50,8 @@ const Datos = () => {
         contometro:'11',
         referencia:'2019758219',
         score:'0'},
-       
-       {institucion:'ELEKTRA NORESTE (ENSA)',
+    
+        {institucion:'ELEKTRA NORESTE (ENSA)',
         tipoObligacion:'SERVICIOS',
         saldoActual:'11.67',
         letra:'11.67',
@@ -59,10 +59,8 @@ const Datos = () => {
         formaPago:'PAGOS A 30 DIAS',
         contometro:'111111111111111111111111',
         referencia:'2017594249',
-        score:'0'},
-       
-
-
-]}
+        score:'0'}
+    ]
+}
 
 module.exports = Datos
