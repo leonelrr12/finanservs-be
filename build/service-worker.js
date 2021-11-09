0f8008365878fd3d7a9b895d5ac1928725f57cf6
Leonel Rodriguez <leonellrodriguez@gmail.com>
2021-11-08 20:02:38 -0500

    Ajuste espaciado de letras en el Landing Page

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe15ff60ce2a0f9327a95ff72147e765.js"
+  "/precache-manifest.e35d63467e280b073b13b9fa15508420.js"
 );
 
 self.addEventListener('message', (event) => {
